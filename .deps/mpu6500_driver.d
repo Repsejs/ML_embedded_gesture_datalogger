ea50c0c1788b27303ba1cc044ba130a72a1fe54e
Repsejs <repsejs@hotmail.com>
2024-07-31 20:03:07 +0200

    implementing timing of sampling

---
+++ .deps/mpu6500_driver.d
@@ -0,0 +1,6 @@
+build/mpu6500_driver.o: lib/mpu/src/mpu6500_driver.c \
+ lib/mpu/include/mpu6500_driver.h lib/mpu/include/mpu6500_registers.h
+
+lib/mpu/include/mpu6500_driver.h:
+
+lib/mpu/include/mpu6500_registers.h:
