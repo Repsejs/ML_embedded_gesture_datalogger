36cee2c6f9d2a164f19d6ec41af841858b5c5a1b
Repsejs <repsejs@hotmail.com>
2024-11-17 22:16:48 +0100

    restructured gesture sampling

--- main.c
+++ main.c
@@ -14,17 +14,28 @@
 #include "tf_card.h"
 
 #define BUFFER_SIZE 512
-#define DESIRED_FREQUENCY_HZ 50
+#define DESIRED_FREQUENCY_HZ 1000
+#define NUMBER_OF_GESTURES 3
+#define MEASURMENTS_OF_EACH_GESTURE 100
+#define MEASURMENTS_PER_GESTURE 100
+//total data points recorded will be NUMBER_OF_GESTURES * MEASURMENTS_OF_EACH_GESTURE * MEASURMENTS_PER_GESTURE 
 
 /* FILENAME should be at most 8 chars long and the extension should be at most 3 */
 
-volatile int16_t last_Sample_buffer[7] = {0};
+volatile int16_t last_Sample_buffer[3] = {0};
+char header[] = "X-acc;Y-acc;Z-acc;label\n";
+char labels[NUMBER_OF_GESTURES][10] = {
+    "idle",
+    "waving",
+    "sliding"
+};
 
 void ledInit (void);
 void led_on_B1();
 void led_off_B1();
 void led_on_B0();
 void led_off_B0();
+void led_on_off_B1();
 void timer_interrupt_config();
 void init_mpu(void);
 int millis(void);
@@ -34,10 +45,11 @@ int int_to_string(char *string, int integer);
 void formatTime(char *output, int minutes, int seconds, int milliseconds);
          
 uint8_t* pDatabuffer = (uint8_t*)&dataBuffer;
-uint32_t time_elapsed = 0;
+uint32_t measurments_taken = 0;
+char current_label[10] = "idle";
 
 int main(void){
-	uint32_t count = 0;
+    uint32_t count = 0;
     char usb_data_buffer[512] = {'\0'};
     char usb_read_buf[80] = {'\0'};
 
@@ -47,11 +59,12 @@ int main(void){
     while (!usb_serial_available()) {
         usb_delay_1ms(100);
     }
-	printf("ready\r\n");
+    printf("\r\nready\r\n");
 
     init_q();
     ledInit();
     usb_delay_1ms(1);
+    uint32_t total_data_points = NUMBER_OF_GESTURES * MEASURMENTS_OF_EACH_GESTURE * MEASURMENTS_PER_GESTURE;
     /* Handle for the mounted filesystem */
     FATFS fs;
 
@@ -70,14 +83,13 @@ int main(void){
 
     /* This function "mounts" the SD-card which makes the filesystem available */
     fr = f_mount(&fs, "test", 1); // Mount storage device
-    f_sync(&fil);
 
     /* This function opens a file. In this case, we are creating a file which we want to write to */
     
     delay_1ms(400);
-	printf("mpu\r\n");
+    printf("initiating MPU\r\n");
     init_mpu();
-	printf("mpu2\r\n");
+    printf("MPU initiated\r\n");
 
     rcu_periph_clock_enable(RCU_DMA0);
 
@@ -89,84 +101,96 @@ int main(void){
     
     int blink = 0;
     int onOff = 0;
-	
-	fr = f_mount(&fs, "test", 1); // Mount storage device
-	f_sync(&fil);
+    
+    fr = f_mount(&fs, "test", 1); // Mount storage device
+    f_sync(&fil);
 
-	fr = f_open(&fil, "TData.CSV", FA_WRITE | FA_CREATE_ALWAYS);
-	clear_queues();
-	/* Write the header to the file */
-	char header[] = "X-acc;Y-acc;Z-acc\n";
-	//clear queue
-	enqueue_string(header);
-	
-	//START IMU DMA
-	//i2c_dma_master_read_register(I2C0, MPU6500_ADDR, MPU6500_ACCEL_XOUT_H);
-	//ENABLE interrupts for data collection
-	timer_interrupt_config();
-	config_clic_irqs();
-	eclic_global_interrupt_enable();  
-	printf("running\r\n");
-    uint64_t start_time = millis();
-    while(time_elapsed < 10000){
-		blink++;
-		if (blink == 1000000){
-			blink = 0;
-			if(onOff){
-				led_on_B0();
-				onOff = 0;
-			}else{
-				led_off_B0();
-				onOff = 1;
-			}
-		}
-		
-		if(queue_str_len() > 5){ //Behövs högre hastighet så kan vi öka denna 5:an
-			memset(write_to_sd, '\0', sizeof(write_to_sd));
-			//printf("x=%d, y=%d, z=%d\r\n", last_Sample_buffer[0],last_Sample_buffer[1],last_Sample_buffer[2]);
-			fflush(0);
-			if (dequeue_string(write_to_sd, 5)) { //Behövs högre hastighet så kan vi öka denna 5:an
-				f_write(&fil, write_to_sd, strlen(write_to_sd), &bw);
-				f_sync(&fil);
-			}
-		}
+    fr = f_open(&fil, "TDATA.CSV", FA_WRITE | FA_CREATE_ALWAYS);
+    clear_queues();
+    f_sync(&fil);
+    //ENABLE interrupts for data collection
 
-	}
-    uint64_t end_time = millis();
-	printf("breaking\r\n");
-    printf("finished in %d (s)\r\n", ((end_time - start_time)/1000.0));
-	timer_interrupt_disable(TIMER1, TIMER_INT_CH0);
-	write_to_sd[0] = '\0';
-	while(empty_string_queue(write_to_sd, 5)){
-		f_write(&fil, write_to_sd, strlen(write_to_sd), &bw); // testa byt strlen till buf_size för bättre prestanda
-		f_sync(&fil);
-		write_to_sd[0] = '\0';
-	}    
+    printf("running\r\n");
+    int start_measurment= 0;
+    for (int gesture = 0; gesture < NUMBER_OF_GESTURES; gesture++) {
+        timer_interrupt_disable(TIMER1, TIMER_INT_CH0);
+        printf("start %s\r\n", labels[gesture]);
+        fflush(0);
+        delay_1ms(5000);
+        start_measurment = 1;
+        for (int repetition = 0; repetition < MEASURMENTS_OF_EACH_GESTURE; repetition++) {
+            snprintf(current_label, sizeof(current_label), "%s", labels[gesture]);
+            printf("Collecting data for gesture: %s, repetition %d\r\n", current_label, repetition + 1);
+            enqueue_string(header);
+            if(start_measurment == 1){
+                timer_interrupt_config();
+                config_clic_irqs();
+                eclic_global_interrupt_enable();  
+            }
+            start_measurment = 0;
 
-	f_close(&fil);
-	printf("done\r\n");
-	fflush(0);
+            while(measurments_taken < MEASURMENTS_PER_GESTURE * (gesture * MEASURMENTS_OF_EACH_GESTURE + repetition + 1)){
+                blink++;
+                if (blink == 1000000){
+                    blink = 0;
+                    if(onOff){
+                        led_on_B0();
+                        onOff = 0;
+                    }else{
+                        led_off_B0();
+                        onOff = 1;
+                    }
+                }
+                
+                if(queue_str_len() > 5){ //Behövs högre hastighet så kan vi öka denna 5:an
+                    memset(write_to_sd, '\0', sizeof(write_to_sd));
+                    //printf("x=%d, y=%d, z=%d\r\n", last_Sample_buffer[0],last_Sample_buffer[1],last_Sample_buffer[2]);
+                    fflush(0);
+                    if (dequeue_string(write_to_sd, 5)) { //Behövs högre hastighet så kan vi öka denna 5:an
+                        f_write(&fil, write_to_sd, strlen(write_to_sd), &bw);
+                        f_sync(&fil);
+                    }
+                }
+            }
+        }
+    }
+
+    printf("Emptying queue\r\n");
+    timer_interrupt_disable(TIMER1, TIMER_INT_CH0);
+    write_to_sd[0] = '\0';
+    while(empty_string_queue(write_to_sd, 5)){
+        f_write(&fil, write_to_sd, strlen(write_to_sd), &bw); // testa byt strlen till buf_size för bättre prestanda
+        f_sync(&fil);
+        write_to_sd[0] = '\0';
+    }    
+
+    f_close(&fil);
+    printf("done\r\n");
+    fflush(0);
 }
 
 
 void TIMER1_IRQHandler(void){
+    led_on_off_B1(measurments_taken%2==0);
     char buf[64] = {'\0'};
     int buf_size = 0;   
-	time_elapsed++;
+    measurments_taken++;
     dma_vector_t vector_write;
-	mpu_vector_t vec;
+    mpu_vector_t vec;
     /* Check which channel triggered the interrupt */
     if(SET == timer_interrupt_flag_get(TIMER1, TIMER_INT_CH0)){
-        
-		mpu6500_getAccel(&vec);
-		last_Sample_buffer[0] = (int16_t)vec.x;
-		last_Sample_buffer[1] = (int16_t)vec.y;
-		last_Sample_buffer[2] = (int16_t)vec.z;
-		append_int_to_string(buf, last_Sample_buffer[0]);
+        mpu6500_getAccel(&vec);
+        last_Sample_buffer[0] = (int16_t)vec.x;
+        last_Sample_buffer[1] = (int16_t)vec.y;
+        last_Sample_buffer[2] = (int16_t)vec.z;
+        append_int_to_string(buf, last_Sample_buffer[0]);
         append_int_to_string(buf, last_Sample_buffer[1]);
         append_int_to_string(buf, last_Sample_buffer[2]);
 
-        append_newLine(buf, strlen(buf)); //Testa byt strlen till buf_size för bättre prestanda
+        // Append label to the string
+        strncat(buf, current_label, sizeof(buf) - strlen(buf) - 1);
+        strcat(buf, "\n");
+
         enqueue_string(buf);
 
         timer_interrupt_flag_clear(TIMER1, TIMER_INT_CH0);
@@ -175,7 +199,7 @@ void TIMER1_IRQHandler(void){
 
 void ledInit (void){
     rcu_periph_clock_enable(RCU_GPIOB);
-	gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_0 | GPIO_PIN_1);
+    gpio_init(GPIOB, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_0 | GPIO_PIN_1);
 }
 
 void led_on_B1(){
@@ -190,6 +214,19 @@ void led_on_B0(){
 }
 void led_off_B0(){
     gpio_bit_write(GPIOB, GPIO_PIN_0, 0);
+}
+void led_on_off_B1(int on_off){
+    gpio_bit_write(GPIOB, GPIO_PIN_1, on_off);
+}
+
+void init_mpu(void){
+    rcu_periph_clock_enable(RCU_GPIOB);
+    rcu_periph_clock_enable(RCU_I2C0);
+    gpio_init(GPIOB, GPIO_MODE_AF_OD, GPIO_OSPEED_50MHZ, GPIO_PIN_6 | GPIO_PIN_7); //led?
+
+    mpu6500_install(I2C0);
+
+    delay_1ms(100);   
 }
 
 void timer_interrupt_config(){
@@ -214,10 +251,10 @@ void timer_interrupt_config(){
     /* initialize timer configuration struct */
     timer_struct_para_init(&timer_initpara);
     /* TIMER1 configuration */
-    /* Frequency = core clock / ((1+prescaler)*period) = 108MHz / (2*54000) = 1KHz */
+    /* Frequency = core clock / ((1+prescaler)*period) = 96MHz / (40 * 48000) = 50Hz */
     
     uint32_t period = SystemCoreClock / (DESIRED_FREQUENCY_HZ * 2);
-    timer_initpara.prescaler         = 1;
+    timer_initpara.prescaler         = 39;
     timer_initpara.alignedmode       = TIMER_COUNTER_EDGE;
     timer_initpara.counterdirection  = TIMER_COUNTER_UP;
     timer_initpara.period            = period;
@@ -293,14 +330,4 @@ void append_newLine(char *string, int length) {
 int millis(void) {
     uint64_t mtime = get_timer_value();
     return ((mtime * 4000.0) / SystemCoreClock); // Adjusted for seconds and milliseconds
-}
-
-void init_mpu(void){
-    rcu_periph_clock_enable(RCU_GPIOB);
-    rcu_periph_clock_enable(RCU_I2C0);
-    gpio_init(GPIOB, GPIO_MODE_AF_OD, GPIO_OSPEED_50MHZ, GPIO_PIN_6 | GPIO_PIN_7); //led?
-
-    mpu6500_install(I2C0);
-
-    delay_1ms(100);   
 }
